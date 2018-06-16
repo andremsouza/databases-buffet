753ad0a8efb37f51055469768e2f263c86f2b050
andremsouza <msouza.andre@hotmail.com>
2018-06-16 01:03:23 -0300

    Updated script to generate insertions. Uses bllsht-my-database for generation.

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Usage: ./run.sh
+# Usage: ./run.sh NUM_INSERTS
 
 # Variáveis de Ambiente
 USER=`whoami`
+ARGS=$1
+DEFAULT_NUM_INSERTS=20
+NUM_INSERTS=${ARGS:-$DEFAULT_NUM_INSERTS}
 DATABASE=database_buffet
-SCHEMA_PATH=./sqlFiles/schema.sql
-OUTPUT=dbDump.sql
+INSERT_OUT=inserts.sql
+SCHEMA_PATH=schema.sql
+GENERATOR_PATH=bllsht-my-database-master/bllshtMyDatabase.py
 ROLE_CHECK=`sudo su -- postgres -c "psql -c \"\du\""`
 DATABASE_CHECK=`sudo su -- postgres -c "psql -c \"\l\""`
 
@@ -31,4 +35,12 @@ sudo su -- postgres -c "psql -c \"CREATE DATABASE $DATABASE;\"" && \
 
 # Criando a estrutura do banco de dados
 echo "Criando a estrutura de $DATABASE..."
-psql $DATABASE < $SCHEMA_PATH
+psql $DATABASE < $SCHEMA_PATH && \
+
+# Gerando os inserts
+echo "Gerando os inserts para o database $DATABASE..."
+python3 $GENERATOR_PATH $SCHEMA_PATH $NUM_INSERTS > $INSERT_OUT && \
+
+# Alimentando o banco de dados com os inserts
+echo "Alimentando $DATABASE com os inserts gerados..."
+psql $DATABASE < $INSERT_OUT
