dfce6fb6264a2da4851dd01d54c773a1cb2281fe
andremsouza <msouza.andre@hotmail.com>
2018-06-22 12:04:30 -0300

    Added dependency installation.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Usage: ./run.sh NUM_INSERTS
 
 # Variáveis de Ambiente
-USER=`whoami`
+USER=buffet
 ARGS=$1
 DEFAULT_NUM_INSERTS=20
 NUM_INSERTS=${ARGS:-$DEFAULT_NUM_INSERTS}
@@ -13,13 +13,27 @@ SCHEMA_PATH=schema.sql
 GENERATOR_PATH=bllsht-my-database-master/bllshtMyDatabase.py
 ROLE_CHECK=`sudo su -- postgres -c "psql -c \"\du\""`
 DATABASE_CHECK=`sudo su -- postgres -c "psql -c \"\l\""`
+UBUNTU_VERSION=xenial # or bionic or trusty
+
+# Dependências
+sudo apt-get update
+sudo apt-get install -y apt-utils gnupg gnupg2 gnupg1 python3 python3-pyqt4 python3-pyqt5 python3-psycopg2
+#apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
+
+# Add PostgreSQL's repository. It contains the most recent stable release
+#     of PostgreSQL, ``10``.
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_VERSION-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y postgresql-10
 
 # Checa se o usuário tem os privilégios necessários
 if echo "$ROLE_CHECK" | grep -q "$USER"; then
 	echo "Usuário \"$USER\" OK. Criando o database $DATABASE..."
 else
 	echo "Definindo permissões para o usuário \"$USER\"..."
-	sudo su -- postgres -c "psql -c \"CREATE ROLE $USER;\"" && \
+	sudo su -- postgres -c "psql -c \"CREATE ROLE $USER WITH PASSWORD 'buffet01';\"" && \
 	sudo su -- postgres -c "psql -c \"ALTER ROLE $USER SUPERUSER;\"" && \
 	sudo su -- postgres -c "psql -c \"ALTER ROLE $USER LOGIN;\"" && \
 	echo "Criando o database $DATABASE..."
@@ -35,12 +49,12 @@ sudo su -- postgres -c "psql -c \"CREATE DATABASE $DATABASE;\"" && \
 
 # Criando a estrutura do banco de dados
 echo "Criando a estrutura de $DATABASE..."
-psql $DATABASE < $SCHEMA_PATH && \
+psql $DATABASE < $SCHEMA_PATH #&& \
 
 # Gerando os inserts
 #echo "Gerando os inserts para o database $DATABASE..."
 #python3 $GENERATOR_PATH $SCHEMA_PATH $NUM_INSERTS > $INSERT_OUT && \
 
 # Alimentando o banco de dados com os inserts
-echo "Alimentando $DATABASE com os inserts gerados..."
-psql $DATABASE < $INSERT_OUT
+#echo "Alimentando $DATABASE com os inserts gerados..."
+#psql $DATABASE < $INSERT_OUT
