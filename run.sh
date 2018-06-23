07109880337ca16b1d8ecd053e9140727fb69f6c
andremsouza <msouza.andre@hotmail.com>
2018-06-23 20:01:51 -0300

    Tweaks and inserts.

--- run.sh
+++ run.sh
@@ -4,29 +4,24 @@
 
 # Variáveis de Ambiente
 USER=buffet
-ARGS=$1
-DEFAULT_NUM_INSERTS=20
-NUM_INSERTS=${ARGS:-$DEFAULT_NUM_INSERTS}
 DATABASE=buffet
 INSERT_OUT=inserts.sql
 SCHEMA_PATH=schema.sql
-GENERATOR_PATH=bllsht-my-database-master/bllshtMyDatabase.py
 ROLE_CHECK=`sudo su -- postgres -c "psql -c \"\du\""`
 DATABASE_CHECK=`sudo su -- postgres -c "psql -c \"\l\""`
 UBUNTU_VERSION=xenial # or bionic or trusty
 
 # Dependências
-sudo apt-get update
-sudo apt-get install -y apt-utils gnupg gnupg2 gnupg1 python3 python3-pyqt4 python3-pyqt5 python3-psycopg2
+sudo apt-get update && apt-get install -y build-essential python3 python3-dev apt-utils gnupg gnupg2 gnupg1 python3-psycopg2
+
 #apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
 
 # Add PostgreSQL's repository. It contains the most recent stable release
 #     of PostgreSQL, ``10``.
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_VERSION-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+sudo add-apt-repository 'http://apt.postgresql.org/pub/repos/apt/xenial-pgdg main'
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y postgresql-10
+sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update && apt-get install -y postgresql-10
 
 # Checa se o usuário tem os privilégios necessários
 if echo "$ROLE_CHECK" | grep -q "$USER"; then
@@ -45,16 +40,12 @@ if echo "$DATABASE_CHECK" | grep -q "$DATABASE"; then
 fi
 
 # Criando o database do zero
-sudo su -- postgres -c "psql -c \"CREATE DATABASE $DATABASE;\"" && \
+sudo su -- postgres -c "psql -c \"CREATE DATABASE $DATABASE OWNER $USER;\"" && \
 
 # Criando a estrutura do banco de dados
 echo "Criando a estrutura de $DATABASE..."
 psql $DATABASE < $SCHEMA_PATH #&& \
 
-# Gerando os inserts
-#echo "Gerando os inserts para o database $DATABASE..."
-#python3 $GENERATOR_PATH $SCHEMA_PATH $NUM_INSERTS > $INSERT_OUT && \
-
 # Alimentando o banco de dados com os inserts
-#echo "Alimentando $DATABASE com os inserts gerados..."
-#psql $DATABASE < $INSERT_OUT
+echo "Alimentando $DATABASE com os inserts gerados..."
+psql $DATABASE < $INSERT_OUT
