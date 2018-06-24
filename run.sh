a979bd2d814d278bea09fc836eeaf3bc0cf1fdd2
andremsouza <msouza.andre@hotmail.com>
2018-06-24 10:53:18 -0300

    Commented dependency lines.

--- run.sh
+++ run.sh
@@ -12,16 +12,16 @@ DATABASE_CHECK=`sudo su -- postgres -c "psql -c \"\l\""`
 UBUNTU_VERSION=xenial # or bionic or trusty
 
 # Dependências
-sudo apt-get update && apt-get install -y build-essential python3 python3-dev apt-utils gnupg gnupg2 gnupg1 python3-psycopg2
+#sudo apt-get update && apt-get install -y build-essential python3 python3-dev apt-utils gnupg gnupg2 gnupg1 python3-psycopg2
 
 #apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
 
 # Add PostgreSQL's repository. It contains the most recent stable release
 #     of PostgreSQL, ``10``.
-sudo add-apt-repository 'http://apt.postgresql.org/pub/repos/apt/xenial-pgdg main'
+#sudo add-apt-repository 'http://apt.postgresql.org/pub/repos/apt/xenial-pgdg main'
 
-sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update && apt-get install -y postgresql-10
+#sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+#sudo apt-get update && apt-get install -y postgresql-10
 
 # Checa se o usuário tem os privilégios necessários
 if echo "$ROLE_CHECK" | grep -q "$USER"; then
